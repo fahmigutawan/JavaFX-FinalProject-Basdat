89d2e8bdfd4e85bd66f6266bf5c8f2366e7389da
fahmirumagutawan <fahmigutawan@gmail.com>
2022-11-21 22:31:50 +0700

    add jdbc URL

--- src/main/java/com/example/javafxfinalprojectbasdat/repository/Repository.java
+++ src/main/java/com/example/javafxfinalprojectbasdat/repository/Repository.java
@@ -1,4 +1,5 @@
 package com.example.javafxfinalprojectbasdat.repository;
 
 public class Repository {
+
 }
---
+++ src/main/java/com/example/javafxfinalprojectbasdat/util/DbUrl.java
@@ -0,0 +1,15 @@
+package com.example.javafxfinalprojectbasdat.util;
+
+public class DbUrl {
+    // Fahmi
+    static String dbUrl = "jdb:sqlserver://DESKTOP-28579ON;databaseName:University";
+
+//    // Nauval
+//    static String dbUrl = "jdb:sqlserver://DESKTOP-28579ON;databaseName:University";
+//
+//    // Arya
+//    static String dbUrl = "jdb:sqlserver://DESKTOP-28579ON;databaseName:University";
+//
+//    // Hasfi
+//    static String dbUrl = "jdb:sqlserver://DESKTOP-28579ON;databaseName:University";
+}
